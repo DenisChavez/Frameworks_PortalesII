9a134a33940de0ddd8c7bad29be78561d144d896
DenisChavez <dabemois2004@gmail.com>
2024-10-02 23:43:49 -0600

    Primer avance del framework Materialize, Tarea #2

---
+++ CafeArtesanal_Materialize/js/script_materialize.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    //variables para que el sideNavBar funcione
+    {
+        let elems = document.querySelectorAll('.sidenav');
+        let instances = M.Sidenav.init(elems);
+    }
+});
